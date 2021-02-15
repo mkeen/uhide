524a8db64f4a94dbad293a49d2ddf942c619b1ec
Mike Keen <mwk@mikekeen.com>
2021-02-14 19:17:45 -0500

    reorg

---
+++ lib/uhide/main.rb
@@ -0,0 +1,162 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'yaml'
+require 'io/console'
+require 'securerandom'
+
+Bundler.require(:default)
+
+☔ = " ☔ > "
+$🔑 = " 🔑 > "
+$🧂 = (lambda { 🗃️ '🧂', SecureRandom.random_bytes })
+$📏 = lambda { 🗃️ '📏', ActiveSupport::MessageEncryptor.key_len }
+INVALID_KEY = 'invalid key'.colorize(:red)
+$cache = {}
+
+def password?
+  🧼 STDIN.noecho(&:gets)
+end
+
+def 🚽
+  print "\r"
+  $stdout.flush
+end
+
+def ❓ 🔑, simple: true
+  print 🔑
+  password = password?
+  $password = password if simple
+  🚽
+  return password
+end
+
+def 🧼 🗑️
+  🗑️.chomp
+end
+
+def 🔓 🏷️: nil
+  if 🏷️.nil?
+    puts $cache['ddllv']
+  else
+    puts $cache['ddllv'][🏷️]
+  end
+  
+  📕 = File.read './🧂'
+  if 📕.empty?
+    🔀 = SecureRandom.random_bytes $📏.call
+    File.write './🧂', Marshal.dump(🔀)
+    return $cache['🧂'] = 🔀
+  else
+    return $cache['🧂'] = Marshal.load(File.read './🧂')
+  end
+  
+end
+
+def 🔒 🏷️, 💎
+  $cache['ddllv'][🏷️] = 💎
+  💾
+end
+
+def 🤖 🗣️
+  👾, 🏷️, 💎 = 🗣️.split
+  
+  case 👾.to_sym
+  when :get
+    if 🏷️
+      🔓 🏷️: 🏷️
+    else
+      🔓
+    end
+    
+  when :set
+    🔒 🏷️, 💎
+    
+  when :reset
+    if ❓($🔑, simple: false) == $password
+      ['ddllv', '🧂', '📏'].each_entry{ |item| File.delete "./#{item}" }
+      exit!
+    else
+      puts INVALID_KEY
+    end
+    
+  end
+  
+end
+
+def 💾
+  ⚛️ = ActiveSupport::MessageEncryptor.new(
+    ActiveSupport::KeyGenerator.new($password).generate_key($🧂.call, $📏.call)  
+  )
+  
+  was_bootstrapped = !(not $cache['ddllv'])
+  📖 = 🗃️ 'ddllv', {}, ⚛️: ⚛️
+  📀 'ddllv', 📖, ⚛️: ⚛️ if was_bootstrapped
+end
+
+def 📀 🗂️, 📖, ⚛️: false
+  if ⚛️
+    📕 = ⚛️.encrypt_and_sign 📖
+  else
+    📕 = 📖
+  end
+  
+  📕 = Marshal.dump(📕) 
+  File.write "./#{🗂️}", 📕
+  return 📕
+end
+
+def 🗃️ 🗂️, 🔀, ⚛️: false
+  if $cache[🗂️]
+    return $cache[🗂️]
+  end
+  
+  begin
+    📕 = File.read "./#{🗂️}"
+  rescue Errno::ENOENT
+    📕 = 📀 🗂️, 🔀, ⚛️: ⚛️
+  end
+  
+  if not 📕.empty?
+    📕 = Marshal.load 📕
+    
+    if ⚛️
+      begin
+        📖 = ⚛️.decrypt_and_verify 📕
+      rescue ActiveSupport::MessageVerifier::InvalidSignature
+        puts INVALID_KEY
+        exit!
+      end
+      
+    else
+      📖 = 📕
+    end
+    
+  end
+  
+  return $cache[🗂️] = 📖
+end
+
+class Uhide::Main
+  class << self
+    def start()
+      begin
+        ❓ $🔑
+        
+        💾
+        
+        loop do
+          print ☔
+          🤖(🧼 gets)
+        end
+        
+      rescue SystemExit, Interrupt
+        🚽
+        exit!
+      end
+      
+    end
+    
+  end
+  
+end
+
--- uhide.gemspec
+++ uhide.gemspec
@@ -5,7 +5,7 @@ Gem::Specification.new do |s|
   s.description      = "Allows a regular person to maintain a named list of secrets that's encrypted at rest"
   s.authors          = ["Mike Keen"]
   s.email            = 'mwk@mikekeen.com'
-  s.files            = ["lib/uhide.rb"]
+  s.files            = ["lib/uhide.rb", "lib/uhide/main.rb"]
   s.bindir           = 'bin'
   s.executables      = ["uhide"]
   s.homepage         = 'https://github.com/mkeen/uhide'
